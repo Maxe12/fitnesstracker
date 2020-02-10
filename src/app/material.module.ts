3b29fd9444d159c45a5bc3d87c3d90a7b0837755
Maximilian Bieleke <Maxbieleke@web.de>
2020-02-10 13:57:24 +0100

    Added email , password field and datepicker with maximum Date (only 18 yo)

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 //Imports from Material modules going in here 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
-    exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
+    imports: [MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule],
+    exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule]
 })
 export class MaterialModule {}
