2f9b27a6908080d75fee457724dc5fa4ad3f60c6
Maxe <maxbieleke@web.de>
2020-05-29 16:22:53 +0200

    added training reducer to root reducer

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -1,15 +1,18 @@
 import * as fromUi from './shared/redux/ui/ui.reducer';
 import * as fromAuth from './shared/redux/auth/auth.reducer';
+import * as fromTraining from './shared/redux/training/training.reducer';
 import {ActionReducerMap, createFeatureSelector, createSelector} from '@ngrx/store';
 
 export interface State {
   ui: fromUi.State;
   auth: fromAuth.State;
+  training: fromTraining.State;
 }
 
 export const reducers: ActionReducerMap<State> = {
   ui: fromUi.uiReducer,
-  auth: fromAuth.authReducer
+  auth: fromAuth.authReducer,
+  training: fromTraining.trainingReducer
 };
 
 export const getUiState = createFeatureSelector<fromUi.State>('ui');
@@ -17,3 +20,8 @@ export const getIsLoading = createSelector(getUiState, fromUi.getIsLoading);
 
 export const getAuthState = createFeatureSelector<fromAuth.State>('auth');
 export const getIsAuthenticated = createSelector(getAuthState, fromAuth.getIsAuthenticated);
+
+export const getTrainingState = createFeatureSelector<fromTraining.State>('training');
+export const getAvailableExercises = createSelector(getTrainingState, fromTraining.getAvailableExercises);
+export const getFinishedExercises = createSelector(getTrainingState, fromTraining.getFinishedExercises);
+export const getActiveExercise = createSelector(getTrainingState, fromTraining.getActiveExercise);
